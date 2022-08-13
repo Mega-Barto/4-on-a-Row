690529eaa4943c3fa26be9b2940d07f81aabe482
Juan Alejandro Pérez Bermúdez <a.perez3@utp.edu.co>
2022-08-13 16:21:07 -0500

    feat: rework in checker; fix: name of PLAYER

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ while not(so in ["clear", "cls"]):#this is a method for select an clear command 
     else:
         print("select a valid option")
 
-while checker() == 0: #From game_mechs
+while checker() == 0 and checker() != 4: #From game_mechs
+    print("Turn of",PLAYER)#Prints turns
     options(0) #First place the possible options for put a mark
     printer(board) #Next is print the board
     cas = int(input("--> ")) #Now an input for select a place for put a mark
@@ -22,16 +23,17 @@ while checker() == 0: #From game_mechs
     for i in range(4): #The program find in the board/array if the number is valid
         for j in range(4):
             if board[i][j] == cas: #if a match is found, the program will put the correspondent mark in his place
-                if turno == 1:
+                if PLAYER == 1:
                     board[i][j] = "X" 
-                elif turno == 2:
+                elif PLAYER == 2:
                     board[i][j] = "O"
                 break
 
-    if turno == 1: #Change of turn
-        turno = 2
-    elif turno == 2:
-        turno = 1
+    if PLAYER == 1: #Change of turn
+        PLAYER = 2
+    elif PLAYER == 2:
+        PLAYER = 1
     cleaner() #Clear the options on the board
     system(so) #Clear the screen
     
+printer(board)#Prints the last board
